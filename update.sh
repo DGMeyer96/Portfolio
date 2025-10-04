4fea8e600c1cb66a17762b852fe974db2ec64e40
Daniel Meyer <dmlegoman96@gmail.com>
2025-10-03 17:31:42 -0700

    Added some echos

--- update.sh
+++ update.sh
@@ -1,5 +1,10 @@
 # Fetches latest from Github Repo, builds, and copies files to appropriate directory
+echo "Get latest changes..."
 git fetch
 git pull
+
+echo "Building..."
 npm run build
+
+echo "Copying files..."
 sudo cp -r dist/. /var/www/portfolio
