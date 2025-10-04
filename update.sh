5b75cff743ca529952762f6de394eefe4d0fc3da
Daniel Meyer <dmlegoman96@gmail.com>
2025-10-03 17:47:01 -0700

    Cleanup

--- update.sh
+++ update.sh
@@ -1,18 +1,12 @@
 #!/bin/sh
 
-# Color variables
-red='\033[0;31m'
+# Color variable
 green='\033[0;32m'
-yellow='\033[0;33m'
-blue='\033[0;34m'
-magenta='\033[0;35m'
-cyan='\033[0;36m'
-
 # Reset color
 clear='\033[0m'
 
 # Fetches latest from Github Repo, builds, and copies files to appropriate directory
-echo -e "${green}Get latest changes...${clear}"
+echo "${green}Get latest changes...${clear}"
 git fetch
 git pull
 
