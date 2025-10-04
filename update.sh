e3677ada18cd06973324729e691a9c191fa9f0dd
Daniel Meyer <dmlegoman96@gmail.com>
2025-10-03 17:45:36 -0700

    Trying to add color to update.sh to make echo easier to see.

--- update.sh
+++ update.sh
@@ -1,10 +1,23 @@
+#!/bin/sh
+
+# Color variables
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+blue='\033[0;34m'
+magenta='\033[0;35m'
+cyan='\033[0;36m'
+
+# Reset color
+clear='\033[0m'
+
 # Fetches latest from Github Repo, builds, and copies files to appropriate directory
-echo "Get latest changes..."
+echo -e "${green}Get latest changes...${clear}"
 git fetch
 git pull
 
-echo "Building..."
+echo "${green}Building...${clear}"
 npm run build
 
-echo "Copying files..."
+echo "${green}Copying files...${clear}"
 sudo cp -r dist/. /var/www/portfolio
