94b55ee1a6e68e0b33a9c361c3473ac381c2fd13
Daniel Meyer <dmlegoman96@gmail.com>
2025-10-03 17:10:23 -0700

    Hide the theme picker, dev only.

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,13 @@ import './index.css'
 import App from './App.tsx'
 import { CssBaseline } from '@mui/material'
 import AppTheme from './theme/AppTheme.tsx'
-import ColorModeSelect from './theme/ColorModeSelect.tsx'
+// import ColorModeSelect from './theme/ColorModeSelect.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AppTheme disableCustomTheme={false}>
       <CssBaseline enableColorScheme />
-      <ColorModeSelect sx={{ position: 'fixed', top: '1rem', right: '1rem' }} />
+      {/* <ColorModeSelect sx={{ position: 'fixed', top: '1rem', right: '1rem' }} /> */}
       <App/>
     </AppTheme>
   </StrictMode>,
